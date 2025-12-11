5eae002a3b66cd632bac6c3f4151bb6103ecd97a
Ruben Sousa <ruben@rubensousa.com>
2025-12-11 23:38:11 +0100

    Rename nextNode to childNode to improve readability

--- src/Day11.kt
+++ src/Day11.kt
@@ -45,11 +45,11 @@ object Day11 {
             }
             val parentNodeVisits = visits.remove(currentNode) ?: continue
             val childNodes = nodes[currentNode] ?: continue
-            childNodes.forEach { nextNode ->
-                val childNodeVisits = visits.getOrDefault(nextNode, 0L)
-                visits[nextNode] = childNodeVisits + parentNodeVisits
+            childNodes.forEach { childNode ->
+                val childNodeVisits = visits.getOrDefault(childNode, 0L)
+                visits[childNode] = childNodeVisits + parentNodeVisits
                 if (childNodeVisits == 0L) {
-                    stack.addLast(nextNode)
+                    stack.addLast(childNode)
                 }
             }
         }
